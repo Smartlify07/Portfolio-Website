c13ba80c651761d792e0b966533cfb16bc2227fc
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-11-14 15:25:19 +0100

    Added filter functionality to projects

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // Show Overlay on hover for projects
 
   const project_card = document.querySelectorAll(".description");
-  console.log(project_card);
   project_card.forEach((project) => {
     project.addEventListener("mouseover", () => {
       project.classList.toggle("active-project");
@@ -28,4 +27,42 @@ document.addEventListener("DOMContentLoaded", () => {
       project.classList.remove("overlay");
     });
   });
+
+  // Filter Projects Based on Languages
+  const projects = document.querySelectorAll(".project");
+  const languages = document.querySelectorAll(".project .languages .btn");
+  languages.forEach((language) => {
+    language.classList.add(language.textContent.toLowerCase());
+  });
+  const filter_btns = document.querySelectorAll(".filter");
+  filter_btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      // console.log(e.target.textContent);
+      const selectedLang = e.target.textContent.toLowerCase();
+      projects.forEach((project) => {
+        projectLang = Array.from(
+          project.querySelectorAll(".languages .btn")
+        ).map((btn) => btn.textContent.toLowerCase());
+
+        console.log(projectLang);
+        if (projectLang.includes(selectedLang)) {
+          project.style.display = "block";
+        } else {
+          project.style.display = "none";
+        }
+      });
+      btn.classList.toggle("btn-dark");
+      toggleBtn(btn);
+    });
+  });
+
+  function toggleBtn(clickedbtn) {
+    filter_btns.forEach((button) => {
+      if (button === clickedbtn) {
+        button.classList.toggle("btn-dark");
+      } else {
+        button.classList.remove("btn-dark");
+      }
+    });
+  }
 });
