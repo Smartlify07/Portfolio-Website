89cc524d83b492fb1f8713873c77636ec04855cb
Obinna Smart Anosike <anosikeobinna895@gmail.com>
2023-11-14 01:01:39 +0100

    Built Homepage for all screens

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //Get burger, navlinks
+  const navSlide = () => {
+    const navlinks = document.querySelector(".nav-links");
+    const burger = document.querySelector(".burger");
+
+    burger.addEventListener("click", () => {
+      // Toggle burger to show X shape
+      burger.classList.toggle("turn");
+      navlinks.classList.toggle("active-nav");
+    });
+  };
+  navSlide();
+
+  // Show Overlay on hover for projects
+
+  const project_card = document.querySelectorAll(".description");
+  console.log(project_card);
+  project_card.forEach((project) => {
+    project.addEventListener("mouseover", () => {
+      project.classList.toggle("active-project");
+      project.classList.toggle("overlay");
+    });
+    project.addEventListener("mouseout", () => {
+      project.classList.remove("active-project");
+      project.classList.remove("overlay");
+    });
+  });
+});
